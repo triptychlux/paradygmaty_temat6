88f9313efd5e6b13c7005dd8c705a8e33ec16349
triptychlux <kacper-94@wp.eu>
2022-06-09 21:12:25 +0200

    pc

--- Temat 6 - Algorytmy na ciagach liczbowych/Form1.Designer.cs
+++ Temat 6 - Algorytmy na ciagach liczbowych/Form1.Designer.cs
@@ -36,13 +36,24 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(14, 101);
+            this.button1.Location = new System.Drawing.Point(12, 100);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(188, 23);
+            this.button1.Size = new System.Drawing.Size(280, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Znajdź najdłuższy wspólny podciąg";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,7 +67,6 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
             this.label3.Size = new System.Drawing.Size(230, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Podaj kolejne liczby ciągu oddzielając je spacją";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // textBox1
             // 
@@ -65,16 +75,15 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(300, 20);
             this.textBox1.TabIndex = 11;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(270, 163);
+            this.label2.Location = new System.Drawing.Point(362, 143);
+            this.label2.MaximumSize = new System.Drawing.Size(300, 300);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 12;
-            this.label2.Text = "label2";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
@@ -97,17 +106,125 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(273, 208);
+            this.label4.Location = new System.Drawing.Point(9, 179);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(0, 13);
             this.label4.TabIndex = 16;
-            this.label4.Text = "label4";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(368, 29);
+            this.textBox3.MaxLength = 3;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(45, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(229, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Podaj ilość liczb pierwszych do wygenerowania";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(365, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Podaj początek ciągu";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(368, 74);
+            this.textBox4.MaxLength = 3;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(45, 20);
+            this.textBox4.TabIndex = 20;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(365, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Wygeneruj ciąg liczb pierwszych";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 22;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 245);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(300, 20);
+            this.textBox5.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(230, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Podaj kolejne liczby ciągu oddzielając je spacją";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 316);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 25;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(280, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Sprawdź czy podany ciąg jest ciągiem liczb pierwszych";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(365, 171);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox6.Size = new System.Drawing.Size(280, 94);
+            this.textBox6.TabIndex = 27;
+            this.textBox6.WordWrap = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(657, 380);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
@@ -115,6 +232,7 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -132,6 +250,17 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- Temat 6 - Algorytmy na ciagach liczbowych/Form1.cs
+++ Temat 6 - Algorytmy na ciagach liczbowych/Form1.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Linq;
 
 namespace Temat_6___Algorytmy_na_ciagach_liczbowych
 {
@@ -17,78 +15,21 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
         {
             InitializeComponent();
         }
-        public void LCS()
+        public bool IsPrime(int n)
         {
-
-            string a = Convert.ToString(textBox1);
-            List<string> strSeq1 = a.Split(' ').ToList();
-            List<int> sequence1 = strSeq1.Select(s => Int32.TryParse(s, out int n) ? n : (int?)null).Where(n => n.HasValue).Select(n => n.Value).ToList();
-            string b = Convert.ToString(textBox2);
-            List<string> strSeq2 = b.Split(' ').ToList();
-            List<int> sequence2 = strSeq2.Select(s => Int32.TryParse(s, out int n) ? n : (int?)null).Where(n => n.HasValue).Select(n => n.Value).ToList();
-
-            int i, j, k, t;
-            int s1Len = sequence1.Count;
-            int s2Len = sequence2.Count;
-            int[] z = new int[(s1Len + 1) * (s2Len + 1)];
-            int[,] c = new int[(s1Len + 1), (s2Len + 1)];
-
-            for (i = 0; i <= s1Len; ++i)
-                c[i, 0] = z[i * (s2Len + 1)];
-
-            for (i = 1; i <= s1Len; ++i)
+            int i;
+            for (i = 2; i <= n - 1; i++)
             {
-                for (j = 1; j <= s2Len; ++j)
+                if (n % i == 0)
                 {
-                    if (sequence1[i - 1] == sequence2[j - 1])
-                    {
-                        c[i, j] = c[i - 1, j - 1] + 1;
-                    }
-                    else
-                    {
-                        if (c[i - 1, j] > c[i, j - 1])
-                        {
-                            c[i, j] = c[i - 1, j];
-                        }
-                        else
-                        {
-                            c[i, j] = c[i, j - 1];
-                        }
-                    }
+                    return false;
                 }
             }
-            t = c[s1Len, s2Len];
-            int[] outputSB = new int[t];
-
-            for (i = s1Len, j = s2Len, k = t - 1; k >= 0;)
+            if (i == n)
             {
-                if (sequence1[i - 1] == sequence2[j - 1])
-                {
-                    outputSB[k] = sequence1[i - 1];
-                    --i;
-                    --j;
-                    --k;
-                }
-                else if (c[i, j - 1] > c[i - 1, j])
-                    --j;
-                else
-                    --i;
+                return true;
             }
-            string outputString = string.Join(" ", outputSB);
-
-            label2.Text = Convert.ToString(outputString);
-
-            label4.Text = Convert.ToString(t);
-
-        }
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
+            return false;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -98,41 +39,142 @@ namespace Temat_6___Algorytmy_na_ciagach_liczbowych
 
         private void button1_Click(object sender, EventArgs e)
         {
-            LCS();
-
-
-
-
-            /*
-             CAŁKOWICIE TESTOWE GÓWNO
+            // najdłuższy wspólny podciąg
 
             string a = Convert.ToString(textBox1);
             List<string> strSeq1 = a.Split(' ').ToList();
             List<int> sequence1 = strSeq1.Select(s => Int32.TryParse(s, out int n) ? n : (int?)null).Where(n => n.HasValue).Select(n => n.Value).ToList();
-            //test
-            int total1 = sequence1.Sum();
+            string b = Convert.ToString(textBox2);
+            List<string> strSeq2 = b.Split(' ').ToList();
+            List<int> sequence2 = strSeq2.Select(s => Int32.TryParse(s, out int n) ? n : (int?)null).Where(n => n.HasValue).Select(n => n.Value).ToList();
+            int i;
+            int j;
+            int k;
+            int l;
+            int seq1L = sequence1.Count;
+            int seq2L = sequence2.Count;
+            int[] arrA = new int[(seq1L + 1) * (seq2L + 1)];
+            int[,] arrB = new int[(seq1L + 1), (seq2L + 1)];
 
+            for (i = 0; i <= seq1L; ++i)
+                arrB[i, 0] = arrA[i * (seq2L + 1)];
 
-            int total2 = 0;
-            int total = 0;
-
-            if (!String.IsNullOrEmpty(textBox2.Text))
+            for (i = 1; i <= seq1L; ++i)
             {
-                string b = Convert.ToString(textBox2);
-                List<string> strSeq2 = b.Split(' ').ToList();
-                List<int> sequence2 = strSeq2.Select(s => Int32.TryParse(s, out int n) ? n : (int?)null).Where(n => n.HasValue).Select(n => n.Value).ToList();
-                //test
-                total2 = sequence2.Sum();
-                total = total1 + total2;
-                label2.Text = Convert.ToString("Dwujeczka GIT " + total);
+                for (j = 1; j <= seq2L; ++j)
+                {
+                    if (sequence1[i - 1] == sequence2[j - 1])
+                    {
+                        arrB[i, j] = arrB[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        if (arrB[i - 1, j] > arrB[i, j - 1])
+                        {
+                            arrB[i, j] = arrB[i - 1, j];
+                        }
+                        else
+                        {
+                            arrB[i, j] = arrB[i, j - 1];
+                        }
+                    }
+                }
+            }
+            l = arrB[seq1L, seq2L];
+            int[] outputArr = new int[l];
+
+            for (i = seq1L, j = seq2L, k = l - 1; k >= 0;)
+            {
+                if (sequence1[i - 1] == sequence2[j - 1])
+                {
+                    outputArr[k] = sequence1[i - 1];
+                    --i;
+                    --j;
+                    --k;
+                }
+                else if (arrB[i, j - 1] > arrB[i - 1, j])
+                    --j;
+                else
+                    --i;
+            }
+            string outputString = string.Join(" ", outputArr);
+            label7.Text = Convert.ToString("Wspólne elementy ciągów: " + outputString);
+            label4.Text = Convert.ToString("Suma wspólnych elementów: " + l);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // generator liczb pierwszych
+
+            int amount = Convert.ToInt32(textBox3.Text);
+            int start = Convert.ToInt32(textBox4.Text);
+            List<int> numbers = new List<int>();
+
+            for (; numbers.Count < amount; start++)
+            {
+                if (IsPrime(start) == true)
+                {
+                    numbers.Add(start);
+                }
+            }
+            string outputString = string.Join(" ", numbers);
+            label2.Text = Convert.ToString("Wygenerowane liczby:");
+            textBox6.Text = Convert.ToString(outputString);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // sprawdzenie ciągu liczb pierwszych
+
+            string a = Convert.ToString(textBox5);
+            List<string> strSeq = a.Split(' ').ToList();
+            List<int> sequence = strSeq.Select(s => Int32.TryParse(s, out int n) ? n : (int?)null).Where(n => n.HasValue).Select(n => n.Value).ToList();
+            int[] arrSeq = sequence.ToArray();
+            int counter = 0;
+            int rise = 0;
+            int ss = 0;
+
+            foreach (int num in arrSeq)
+            {
+                if (num <= 1)
+                {
+                    counter--;
+                }
+                for (int i = 2; i < Math.Sqrt(num); i++)
+                {
+                    if (num % i == 0)
+                    {
+                        counter--;
+                    }
+                }
+                counter++;
+                if (ss > 0)
+                {
+                    if (arrSeq[ss] > arrSeq[ss - 1])
+                    {
+                        rise++;
+                    }
+                }
+                ss++;
+            }
+            if (counter == arrSeq.Length && (rise + 1) == arrSeq.Length)
+            {
+                label9.Text = "Podany ciąg jest ciągiem liczb pierwszych i jest rosnący";
+            }
+            else if (counter == arrSeq.Length)
+            {
+                label9.Text = "Podany ciąg jest ciągiem liczb pierwszych ale nie jest stale rosnący";
             }
             else
             {
-                total = total1 + total2;
-                label2.Text = Convert.ToString("Dwujeczka pusta " + total);
+                label9.Text = "To nie jest ciąg liczb pierwszych";
             }
-
-            */
         }
     }
 }
+
